0d901e3ef80abeba1ba12578aa95e04b6663d57d
ChrisTrenkamp <chris@christrenkamp.com>
2016-02-13 17:37:53 -0500

    Merging with master

--- goxpath/marshal.go
+++ goxpath/marshal.go
@@ -91,25 +91,31 @@ func encNS(ns xml.Attr, e *xml.Encoder) error {
 }
 
 func encEle(val xml.StartElement, n tree.Node, e *xml.Encoder) error {
-	for i := 0; i < len(val.Attr); i++ {
-		if val.Attr[i].Name.Local == "xmlns" || val.Attr[i].Name.Space == "xmlns" {
-			val.Attr = append(val.Attr[:i], val.Attr[i+1:]...)
-			i--
+	if n.GetParent() != n {
+		for i := 0; i < len(val.Attr); i++ {
+			if val.Attr[i].Name.Local == "xmlns" || val.Attr[i].Name.Space == "xmlns" {
+				val.Attr = append(val.Attr[:i], val.Attr[i+1:]...)
+				i--
+			}
 		}
-	}
 
-	err := e.EncodeToken(val)
-	if err != nil {
-		return err
+		err := e.EncodeToken(val)
+		if err != nil {
+			return err
+		}
 	}
 
 	if x, ok := n.(tree.Elem); ok {
 		for _, i := range x.GetChildren() {
-			err = encTok(i, e)
+			err := encTok(i, e)
 			if err != nil {
 				return err
 			}
 		}
+	}
+
+	if n.GetParent() == n {
+		return nil
 	}
 
 	return e.EncodeToken(xml.EndElement{Name: val.Name})
